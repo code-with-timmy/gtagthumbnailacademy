1fab98dc37256fe652aa74a6cc49d170c33e4189
Timmy <ojotimothy04@gmail.com>
2025-12-19 14:46:37 +0100

    new

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,10 @@ import Home from "./pages/Home";
 import Signin from "./pages/Authentication/Signin";
 import Signup from "./pages/Authentication/Signup";
 import Course from "./pages/Course";
-import { Upload } from "lucide-react";
+// import { Upload } from "lucide-react";
 import Purchase from "./pages/Purchase";
 // import Assets from "./pages/Assets";
-import AdminKofi from "./pages/AdminKofi";
+// import AdminKofi from "./pages/AdminKofi";
 import AppLayout from "./components/ui/AppLayout";
 import ProtectedRoute from "./components/ui/ProtectedRoute";
 import ProtectedRouteForCourse from "./components/ui/ProtectedRouteForCourse";
@@ -47,9 +47,9 @@ function App() {
             <Route element={<ProtectedRouteForCourse />}>
               <Route path="course" element={<Course />} />
               {/* <Route path="assets" element={<Assets />} /> */}
-              <Route path="upload" element={<Upload />} />
+              {/* <Route path="upload" element={<Upload />} /> */}
             </Route>
-            <Route path="adminKofi" element={<AdminKofi />} />
+            {/* <Route path="adminKofi" element={<AdminKofi />} /> */}
           </Route>
 
           {/* GROUP C: Public Routes */}
